24a861b7e3226c6c274ede0fac6cbf114435b6e4
Ahmed Elhaddad <5328478+aihaddad@users.noreply.github.com>
2020-05-07 12:00:08 +0000

    Building Docker with Jenkins

---
+++ jenkins/main.tf
@@ -0,0 +1,24 @@
+resource "docker_volume" "jenkins_volume" {
+  name = "jenkins_data"
+}
+
+resource "docker_container" "jenkins_container" {
+  name  = "jenkins"
+  image = "jenkins:terraform"
+
+  ports {
+    internal = "8080"
+    external = "8080"
+  }
+
+  volumes {
+    volume_name    = "${docker_volume.jenkins_volume.name}"
+    container_path = "/var/jenkins_home"
+  }
+
+  volumes {
+    host_path      = "/var/run/docker.sock"
+    container_path = "/var/run/docker.sock"
+  }
+}
+
